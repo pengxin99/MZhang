2bd5f2aa36160f1fa2e0ae98a72f850fd52768d6
pengxin99 <yc316ypx@126.com>
2018-05-25 22:39:11 +0800

    change

--- 4class/MZ_softmax_byKeras.py
+++ 4class/MZ_softmax_byKeras.py
@@ -2,11 +2,13 @@ import numpy as np
 
 # np.random.seed(1337)  # for reproducibility
 from keras.datasets import mnist
+import keras
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Convolution2D, MaxPooling2D
 from keras.utils import np_utils
 from keras import backend as K
+import matplotlib.pyplot as plt
 
 
 from keras.models import load_model
@@ -36,6 +38,44 @@ def makeModel(kernel_size1, kernel_size2):
     model.add(Activation('softmax'))  # Softmax评分
     
     return model
+
+#写一个LossHistory类，保存loss和acc
+class LossHistory(keras.callbacks.Callback):
+    def on_train_begin(self, logs={}):
+        self.losses = {'batch':[], 'epoch':[]}
+        self.accuracy = {'batch':[], 'epoch':[]}
+        self.val_loss = {'batch':[], 'epoch':[]}
+        self.val_acc = {'batch':[], 'epoch':[]}
+
+    def on_batch_end(self, batch, logs={}):
+        self.losses['batch'].append(logs.get('loss'))
+        self.accuracy['batch'].append(logs.get('acc'))
+        self.val_loss['batch'].append(logs.get('val_loss'))
+        self.val_acc['batch'].append(logs.get('val_acc'))
+
+    def on_epoch_end(self, batch, logs={}):
+        self.losses['epoch'].append(logs.get('loss'))
+        self.accuracy['epoch'].append(logs.get('acc'))
+        self.val_loss['epoch'].append(logs.get('val_loss'))
+        self.val_acc['epoch'].append(logs.get('val_acc'))
+
+    def loss_plot(self, loss_type):
+        iters = range(len(self.losses[loss_type]))
+        plt.figure()
+        # acc
+        plt.plot(iters, self.accuracy[loss_type], 'r', label='train acc')
+        # loss
+        plt.plot(iters, self.losses[loss_type], 'g', label='train loss')
+        if loss_type == 'epoch':
+            # val_acc
+            plt.plot(iters, self.val_acc[loss_type], 'b', label='val acc')
+            # val_loss
+            plt.plot(iters, self.val_loss[loss_type], 'k', label='val loss')
+        plt.grid(True)
+        plt.xlabel(loss_type)
+        plt.ylabel('acc-loss')
+        plt.legend(loc="upper right")
+        plt.show()
 
 
 if __name__ == '__main__':
@@ -89,7 +129,7 @@ if __name__ == '__main__':
     Y_test = np_utils.to_categorical(y_test[0], nb_classes)
     print('Y_train shape:', Y_train.shape)
     print('Y_test shape:', Y_test.shape)
-
+    '''
     
     # 构建模型，序贯模型
     model = Sequential()
@@ -97,30 +137,55 @@ if __name__ == '__main__':
     
     # 编译模型
     model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
-    # 训练模型
-    model.fit(X_train, Y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(X_test, Y_test))
-    
+    # 创建一个实例history
+    history = LossHistory()
+    # 训练模型（注意这个地方要加入callbacks）
+    model.fit(X_train, Y_train, batch_size=batch_size, epochs=20, verbose=1, validation_data=(X_test, Y_test), callbacks=[history])
+   
     # 评估模型
     score = model.evaluate(X_test, Y_test, verbose=0)
     
     # 保存模型
-    model.save('my_model.h5')   # HDF5 file, you have to pip3 install h5py if don't have it
+    # model.save('my_model.h5')   # HDF5 file, you have to pip3 install h5py if don't have it
     
     '''
     
     # 从加载的模型进行预测
     
-    model = load_model('my_model.h5')
+    model = load_model('my_model_acc0.995.h5')
     score = model.evaluate(X_test, Y_test, verbose=0)
-    '''
+    
     
     print('Test score:', score[0])
     print('Test accuracy:', score[1])
     
-    
     prediction = model.predict(X_test)
+    
+    # 预测的输出结果进行标准化，因为预测输出结果是每个分类的概率，这里转换为[1,0,0,0] one-hot 形式
+    for i in range(prediction.shape[0]):
+        index = 0
+        temp = 0
+        for j in range(prediction[i].shape[0]):
+            if( prediction[i][j] > temp):
+                temp = prediction[i][j]
+                index = j
+        for j in range(prediction[i].shape[0]):
+            if j != index:
+                prediction[i][j] = 0
+            else:
+                prediction[i][j] = 1
+        
+        # print(prediction[i])
+        
+   
+    
+    
     print_mislabeled_images(X_test, Y_test, prediction)       # 显示错误分类的图像
     
     # 可视化模型（模型结构）
-    # plot_model(model, to_file='cnn+softmax_model.png', show_shapes = True)
+    plot_model(model, to_file='cnn+softmax_model.png', show_shapes = True)
 
+    
+
+    # 绘制acc-loss曲线
+    # history.loss_plot('epoch')
